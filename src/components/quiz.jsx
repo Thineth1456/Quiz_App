e85c2bca688888ab2a80594cd29aa24b7da457d5
Thineth1456 <thinethlakshan1456@gmail.com>
2025-12-04 23:49:57 +0530

    Prepare questions

---
+++ src/components/quiz.jsx
@@ -0,0 +1,46 @@
+function Quiz(){
+  const questions = [
+  {
+    id: 1,
+    question: "Which language runs in a web browser?",
+    options: ["Java", "C", "Python", "JavaScript"],
+    answer: "JavaScript"
+  },
+  {
+    id: 2,
+    question: "What does CSS stand for?",
+    options: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats"
+    ],
+    answer: "Cascading Style Sheets"
+  },
+  {
+    id: 3,
+    question: "What does HTML stand for?",
+    options: [
+      "Hypertext Markup Language",
+      "Hyperloop Machine Language",
+      "Hyperlink and Text Markup Language",
+      "Home Tool Markup Language"
+    ],
+    answer: "Hypertext Markup Language"
+  },
+  {
+    id: 4,
+    question: "What year was JavaScript launched?",
+    options: ["1996", "1995", "1994", "None of the above"],
+    answer: "1995"
+  },
+  {
+    id: 5,
+    question: "Which of these is a JavaScript framework?",
+    options: ["React", "Laravel", "Django", "Flask"],
+    answer: "React"
+  }
+];
+
+
+}
